1c4d1398157f89fad920e51d24feb1fe1d36605a
Marianna <janczak.mariannal@gmail.com>
2018-07-09 19:45:37 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    MainFooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
